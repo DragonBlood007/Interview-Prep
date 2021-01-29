9e012ff674bda1e96af694231cbcebf97f34aae5
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-01-29 06:09:38 +0530

    Create 2.cpp

---
+++ Sheet/Day1/2.cpp
@@ -0,0 +1,20 @@
+// Link :- https://leetcode.com/problems/sort-colors/
+// Ques - Sort array of 0's,1's and 2's
+
+void sortColors(vector<int>& nums) {
+    int i = 0,low = 0, high = nums.size() - 1;
+    while(i <= high){
+        if(nums[i] == 0){
+            swap(nums[i],nums[low]);
+            i++;
+            low ++;
+        }
+        else if(nums[i] == 2){
+            swap(nums[i], nums[high]);
+            high--;
+        }
+        else
+            i++;
+
+    }
+}
