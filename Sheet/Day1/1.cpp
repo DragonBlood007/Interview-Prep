9418505bcad764f66de647cc4ff47ff044061fd6
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-01-29 06:11:31 +0530

    Update 1.cpp

--- Sheet/Day1/1.cpp
+++ Sheet/Day1/1.cpp
@@ -1,7 +1,9 @@
 // Link - https://leetcode.com/problems/find-the-duplicate-number/
 
-//Code :-
+//Idea :- Check if duplicate no. lies in [1,n/2] by counting no.s <= n/2 in array
+// If cnt > n/2, duplicate in [1,n/2] else in [n/2+1,n]
 
+//Code :-
 int findDuplicate(vector<int>& nums) {
     int low = 1, high = nums.size()-1;
     while(low < high){
