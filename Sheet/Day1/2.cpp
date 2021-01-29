ed07277a6509dd930f879a8a857ce8de840034ac
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-01-29 06:13:25 +0530

    Update 2.cpp

--- Sheet/Day1/2.cpp
+++ Sheet/Day1/2.cpp
@@ -1,6 +1,9 @@
 // Link :- https://leetcode.com/problems/sort-colors/
 // Ques - Sort array of 0's,1's and 2's
 
+//Idea :- Put 0's at lef side and 2's at right side
+
+
 void sortColors(vector<int>& nums) {
     int i = 0,low = 0, high = nums.size() - 1;
     while(i <= high){
@@ -12,6 +15,7 @@ void sortColors(vector<int>& nums) {
         else if(nums[i] == 2){
             swap(nums[i], nums[high]);
             high--;
+            // since the swapped no. might be 2 we can't mpve ahead before sorting it, hence no i++
         }
         else
             i++;
