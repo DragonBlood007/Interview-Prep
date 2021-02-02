685a4128810e04cd981902487a56698232354a95
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-02-02 16:20:28 +0530

    Create 6.cpp

---
+++ Sheet/Day2/6.cpp
@@ -0,0 +1,16 @@
+//Link - 
+//Idea - To rortate the matrix by 90 degress, first take the transpose and then reverse every row
+
+//Code
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for(int i = 0;i < n;i++){
+        for(int j = i+1;j < n;j++){
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for(int i = 0;i < n;i++){
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
