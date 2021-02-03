552d8eaa1e4273aabdbd242b4b4832d03ab9a3c0
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-02-03 20:27:38 +0530

    Create 1.cpp

---
+++ Sheet/Day3/1.cpp
@@ -0,0 +1,19 @@
+//Link - 
+
+//Idea - Start from lower left and move up if the current element is greater or right if current element is lesser
+
+//Code
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size(), n = matrix[0].size();
+    int i = m-1, j = 0;
+    while(i >= 0 && j < n){
+        if(matrix[i][j] == target)
+            return true;
+        if(matrix[i][j] > target)
+            i--;
+        else
+            j++;
+    }
+    return false;
+}
