176bdac07aa00e0d7b0b15afcf7a49eb9c91c146
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-02-08 12:08:04 +0530

    Create 2.cpp

---
+++ Sheet/Day5/2.cpp
@@ -0,0 +1,13 @@
+//Link - https://leetcode.com/problems/middle-of-the-linked-list/
+
+//Idea - Use fast and slow pointer
+
+//Code
+ListNode* middleNode(ListNode* head) {
+  ListNode *fast = head, *slow;
+  while(fast && fast -> next){
+      fast = fast -> next -> next;
+      slow = slow -> next;
+  }
+  return slow;
+}
