24ae40ae5b312ec9bb7a5da991a33e1a2551ef97
Abhijeet Dwivedi <44425107+DragonBlood007@users.noreply.github.com>
2021-02-23 21:32:25 +0530

    Create first missing positive number.cpp

---
+++ LC Patterns Solutions/Arrays/first missing positive number.cpp
@@ -0,0 +1,17 @@
+//Link - https://leetcode.com/problems/first-missing-positive/
+
+int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+    int i = 0;
+    while(i < n){
+        int j = nums[i] - 1LL;
+        if(j >= 0 && j < n && nums[i] != nums[j])
+            swap(nums[i], nums[j]);
+        else i++;
+    }
+    for(int i = 0;i < n;i++){
+        if(nums[i] != i+1)
+            return i+1;
+    }
+    return n+1;
+}
